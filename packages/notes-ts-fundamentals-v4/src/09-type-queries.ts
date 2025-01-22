248f126d65ff045493d13db687ff5485bea419aa
Dima S <dima@sukhikh.xyz>
2025-01-22 10:08:52 +0100

    Type Queries

--- packages/notes-ts-fundamentals-v4/src/09-type-queries.ts
+++ packages/notes-ts-fundamentals-v4/src/09-type-queries.ts
@@ -1,49 +1,38 @@
 //* keyof
 
 type DatePropertyNames = keyof Date
-/*
-// type DateStringPropertyNames = DatePropertyNames & string
-// type DateSymbolPropertyNames = DatePropertyNames & symbol
 
-//* typeof
-/*
-// async function main() {
-//     const apiResponse = await Promise.all([
-//         fetch("https://example.com"),
-//         Promise.resolve("Titanium White"),
-//     ])
-//     type ApiResponseType = typeof apiResponse
-// }
-/*
-//?^ note: type alias within a function scope!
+type DateStringPropertyNames = DatePropertyNames & string
+type DateSymbolPropertyNames = DatePropertyNames & symbol
+
+async function main() {
+  const apiResponse = await Promise.all([
+    fetch('https://example.com'),
+    Promise.resolve('Titanium White'),
+  ])
+  type ApiResponseType = typeof apiResponse
+}
+
 // const MyAjaxConstructor = CSSRule
 // CSSRule.STYLE_RULE
 // const myAjax = new CSSRule()
 
+interface Car {
+  make: string
+  model: string
+  year: number
+  color: {
+    red: string
+    green: string
+    blue: string
+  }
+}
 
-//* Indexed Access Types
+let carColor: Car['color'] //✔️ Reaching for something that exists
+// let carSomething: Car['not-something-on-car'] //! Reaching for something invalid
+let carColorRedComponent: Car['color']['red'] //✔️ Reaching for something nested
+let carProperty: Car['color' | 'year'] // ✔️ Passing a union type through the index
 
-/*
-// interface Car {
-//     make: string
-//     model: string
-//     year: number
-//     color: {
-//         red: string
-//         green: string
-//         blue: string
-//     }
-// }
+import('./09-type-registry/')
 
-// let carColor: Car["color"] //✔️ Reaching for something that exists
-// let carSomething: Car["not-something-on-car"] //! Reaching for something invalid
-// let carColorRedComponent: Car["color"]["red"] //✔️ Reaching for something nested
-// let carProperty: Car["color" | "year"] // ✔️ Passing a union type through the index
-
-//* Use case: the type registry pattern
-/*
-// See: 
-import("./09-type-registry/")
-
-/**/
 export default {}
